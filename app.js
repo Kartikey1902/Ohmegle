b9e9e496979180300369dd686e2e303c69bccc7b
Kartikey Awasthi <awasthi.kartikey1902@gmail.com>
2024-10-21 17:51:53 +0530

    public path resolve

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ io.on("connection", (socket) => {
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/Public")));
 
 const indexRouter = require("./routes/index.js");
 
